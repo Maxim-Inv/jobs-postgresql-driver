454a82e8d3bed2a342292a869e77b89148b61bd3
Maxim Anisimov <maxim@invoodoo.com>
2020-04-14 16:30:27 +0300

    Extract JobModel to separate file

---
+++ Sources/JobsPostgreSQLDriver/JobModel.swift
@@ -0,0 +1,79 @@
+//
+//  File.swift
+//  
+//
+//  Created by Maxim Anisimov on 14.04.2020.
+//
+
+import Vapor
+import Fluent
+import Queues
+
+enum JobState: String, Codable {
+    case pending = "pending"
+    case processing = "processing"
+    case completed = "completed"
+}
+
+public final class JobModel: Model, Content {
+    
+    public static let schema = "job"
+    
+    @ID(custom: "id")
+    public var id: Int?
+    
+    /// The Job key
+    @Field(key: "key")
+    var key: String
+    
+    /// The unique Job uuid
+    @Field(key: "job_id")
+    var job_id: String
+    
+    /// The Job data
+    @Field(key: "data")
+    var data: Data
+    
+    /// The current state of the Job
+    @Field(key: "state")
+    var state: String
+    
+    /// The created timestamp
+    @Timestamp(key: "created_at", on: .create)
+    var createdAt: Date?
+    
+    /// The updated timestamp
+    @Timestamp(key: "updated_at", on: .update)
+    var updatedAt: Date?
+    
+    @Timestamp(key: "deleted_at", on: .delete)
+    var deletedAt: Date?
+    
+    /// Codable keys
+    enum CodingKeys: String, CodingKey {
+        case id
+        case key
+        case jobId = "job_id"
+        case data
+        case state
+        case createdAt = "created_at"
+        case updatedAt = "updated_at"
+    }
+    
+    public init() {
+    }
+    
+    init(key: String,
+         jobId: String,
+         data: Data,
+         state: JobState = .pending,
+         createdAt: Date = Date(),
+         updatedAt: Date = Date()) {
+        self.key = key
+        self.job_id = jobId
+        self.data = data
+        self.state = state.rawValue
+        self.createdAt = createdAt
+        self.updatedAt = updatedAt
+    }
+}
--- Sources/JobsPostgreSQLDriver/Migrations/CreateJobModel.swift
+++ Sources/JobsPostgreSQLDriver/Migrations/CreateJobModel.swift
@@ -5,7 +5,7 @@ public struct CreateJob: Migration {
     public init() {}
     
     public func prepare(on database: Database) -> EventLoopFuture<Void> {
-        return database.schema("job")
+        return database.schema(JobModel.schema)
             .field("id", .int, .identifier(auto: true))
             .field("key", .string, .required)
             .field("job_id", .string, .required)
@@ -13,10 +13,11 @@ public struct CreateJob: Migration {
             .field("state", .string, .required)
             .field("created_at", .datetime)
             .field("updated_at", .datetime)
+            .field("deleted_at", .datetime)
             .create()
     }
 
     public func revert(on database: Database) -> EventLoopFuture<Void> {
-        return database.schema("job").delete()
+        return database.schema(JobModel.schema).delete()
     }
 }
