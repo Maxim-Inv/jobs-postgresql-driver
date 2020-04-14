fae0ad56922ed31aadc79869d4972922445ed94a
Maxim Anisimov <maxim@invoodoo.com>
2020-04-14 14:17:46 +0300

    Update package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.2
 
 import PackageDescription
 
@@ -13,15 +13,18 @@ let package = Package(
       targets: ["JobsPostgreSQLDriver"]),
     ],
   dependencies: [
-    .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-beta.3.10"),
-    .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0-beta.2.4"),
-    .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.0.0-beta.2.3"),
-    .package(url: "https://github.com/vapor-community/jobs.git", from: "1.0.0-beta.3"),
+    .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-rc"),
+    .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0-rc"),
+    .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.0.0-rc"),
+    .package(url: "https://github.com/vapor/queues.git", from: "1.0.0-rc"),
   ],
   targets: [
-    .target(
-      name: "JobsPostgreSQLDriver",
-      dependencies: ["Vapor", "Jobs", "FluentPostgresDriver", "Fluent"]),
+    .target(name: "JobsPostgreSQLDriver", dependencies: [
+		.product(name: "Vapor", package: "vapor"),
+		.product(name: "Fluent", package: "fluent"),
+		.product(name: "FluentPostgresDriver", package: "fluent-postgres-driver"),
+		.product(name: "Queues", package: "queues")
+	]),
     .testTarget(
       name: "JobsPostgreSQLDriverTests",
       dependencies: ["JobsPostgreSQLDriver"]),
